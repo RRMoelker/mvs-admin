9192ca5d5f201f260925e10a84f1ff79845b00ef
Ruurd Moelker <RRMoelker@users.noreply.github.com>
2017-09-09 22:13:02 +0200

    Very basic D3 rendering

--- src/components/challenges/challenges.component.js
+++ src/components/challenges/challenges.component.js
@@ -1,6 +1,8 @@
 import { formatTime } from '../../util/format.js';
 import { ALMOST_THRESHOLD } from '../../constants.js';
 
+import * as d3 from 'd3';
+
 const ownerDocument = document.currentScript.ownerDocument;
 const rowTemplate = ownerDocument.querySelector('#row');
 
@@ -23,6 +25,13 @@ class ChallengesComponent extends HTMLElement {
         ];
     }
 
+    connectedCallback () {
+        // Called when the element is inserted into a document, including into a shadow tree
+        console.log(this.constructor.name, 'connected');
+
+        this.container = d3.select('.js-container');
+    }
+
     attributeChangedCallback (name, oldValue, newValue) {
         // Called when an attribute is changed, appended, removed,
         // or replaced on the element. Only called for observed attributes.
@@ -39,32 +48,59 @@ class ChallengesComponent extends HTMLElement {
     }
 
     draw() {
-
-        const rowContainerEl = this.querySelector('.js-container');
-
-        while (rowContainerEl.firstChild) {
-            rowContainerEl.removeChild(rowContainerEl.firstChild);
+        const someData = Object.entries(this.params.list);
+        const table = [];
+        for( const [ key, value ] of someData ) {
+            table.push([key, formatTime(value.remaining), formatTime(value.until)]);
         }
-        for( const [ key, value ] of Object.entries(this.params.list) ) {
-            const row = document.importNode(rowTemplate.content, true);
-            row.querySelector('.js-label').innerHTML = key;
 
-            const remainingEl = row.querySelector('.js-remaining');
-            remainingEl.innerHTML = formatTime(value.remaining);
 
-            if ( value.remaining < ALMOST_THRESHOLD) {
-                remainingEl.classList.add('c-challenges__challenge--almost');
-            }
+        // DATA JOIN
+        // Join new data with old elements, if any.
+        const join = this.container
+            .selectAll('tr')
+                .data(table, d => d[0]);
 
-            row.querySelector('.js-until').innerHTML = formatTime(value.until);
+        // UPDATE
+        // Update old elements as needed.
 
-            const recent = this.params.recent[key];
-            if (recent) {
-                row.querySelector('.js-added').innerHTML = '+ ' + formatTime(recent);
-            }
+        // ENTER
+        // Create new elements as needed.
+        //
+        const d3Enter = join.enter()
+            .append('tr')
+            // .style("color", "green") // make the body green
+            // .transition(t).style("opacity", 0);
+            .attr('class', 'c-challenges__challenge--enter');
 
-            rowContainerEl.appendChild(row);
-        }
+        // ENTER + UPDATE
+        // After merging the entered elements with the update selection,
+        // apply operations to both.
+        const innerJoin = d3Enter.merge(join)
+            .selectAll('td')
+                .data(r => r);
+
+        const d3InnerEnter =
+            innerJoin.enter()
+                .append('td')
+                    .text(d => d)
+                    .classed('enter', true);
+
+        d3InnerEnter.merge(innerJoin)
+            .classed('enter', false)
+            .text(d => d);
+
+        // EXIT
+        // Remove old elements as needed.
+        const exit = join.exit();
+
+        const exitTime = 1000; // 1s
+        join.exit()
+            .attr('class', 'c-challenges__challenge--exit')
+            .transition().delay( exitTime )
+            .remove();
+        // console.log('exit: ', exit.size());
+
     }
 }
 
