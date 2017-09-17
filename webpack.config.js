5532bd2d82332527060c69bb06da8697a1ca99e4
Ruurd Moelker <RRMoelker@users.noreply.github.com>
2017-09-17 13:10:21 +0200

    Autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
+const autoprefixer = require('autoprefixer');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const webpack = require('webpack');
-const path = require('path');
 const glob = require('glob');
+const path = require('path');
+const webpack = require('webpack');
 
 const PROJECT_DIR = __dirname;
 const SRC_DIR = path.resolve(PROJECT_DIR, 'src');
@@ -70,6 +71,13 @@ module.exports = {
                     loader: 'style-loader' // creates style nodes from JS strings
                 }, {
                     loader: 'css-loader' // translates CSS into CommonJS
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: function () {
+                            return [autoprefixer];
+                        }
+                    }
                 }, {
                     loader: 'sass-loader' // compiles Sass to CSS
                 }]
